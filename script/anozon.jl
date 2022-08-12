e50906514cb35ca088cd6792b039cea88ed52e97
elzup <guild0105@gmail.com>
2022-08-12 23:16:02 +0900

    add matrix

--- script/anozon.jl
+++ script/anozon.jl
@@ -1,4 +1,5 @@
 import Base.Iterators: flatten, partition
+using LinearAlgebra
 
 alphabet = 'a':'z'
 
@@ -8,11 +9,23 @@ anoz = partition(alphabet, 14) |>
 
 z = map(v -> v[1:3], [anoz, reverse(anoz)]) |> join
 
-print(z)
+println(z)
 
 # mat
 pad = (a, h, l) -> [fill(h, l - size(a)[1]); a]
 pad7c = a -> pad(a, '-', 7)
-l = partition(alphabet, 7) |> collect
 
-map(pad7c, partition(alphabet, 7))
+ord = c -> c |> codepoint |> Int
+m = map(pad7c, partition(alphabet, 7)) |>
+    flatten |>
+    a -> map(ord, a) |>
+         collect |>
+         a -> reshape(a, 7, 4)
+
+l = [1; zeros(Int, 6, 1)]
+r = reverse(l)
+s = [l r] |> m -> [m m]
+
+anoz2 = ones(Int, 1, 7) * (m .* s) |> l -> map(Char, l) |> join
+z = map(v -> v[1:3], [anoz2, reverse(anoz2)]) |> join
+println(z)
